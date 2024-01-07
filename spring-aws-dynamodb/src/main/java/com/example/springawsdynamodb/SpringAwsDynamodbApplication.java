2460b818d9f0fed4ca59a733e1f7c31f70a76ea3
mertcakmakk <mert.cakmak@getir.com>
2024-01-07 17:57:10 +0300

    spring-aws-dynamodb

--- spring-aws-dynamodb/src/main/java/com/example/springawsdynamodb/SpringAwsDynamodbApplication.java
+++ spring-aws-dynamodb/src/main/java/com/example/springawsdynamodb/SpringAwsDynamodbApplication.java
@@ -17,27 +17,30 @@ public class SpringAwsDynamodbApplication {
     @Bean
     public ApplicationRunner runner(UserRepository userRepository){
         return args -> {
-            log.debug("Spring Boot Aws DynamoDB integration");
+            log.info("Spring Boot Aws DynamoDB integration");
 
             // Save User
             var address = Address.builder().city("Test City").street("Test Street").build();
-            var user = User.builder().address(address).name("user name").surname("user surname").age(25).build();
+            var user = User.builder().address(address).username("user name").surname("user surname").age(25).build();
             var savedUser = userRepository.insertUser(user);
 
             var id = savedUser.getId();
 
-            // Update the Name field
-            user.setName("edit name");
-            userRepository.updateUserById(id, user);
+            // Update the user
+            user.setAge(18);
+            userRepository.updateUser(id, user);
 
             // Find User By Id
             var existUser = userRepository.findUserById(id);
-            log.info(String.format("Exist User Name: %s", existUser.getName()));
+            log.info(String.format("Exist User Name: %s", existUser.getUsername()));
 
             log.info(id);
 
+            userRepository.scanUserByBeginsWithUserName("edit");
+            userRepository.scanUserByAgeLessThan(20);
+
             // Will delete saved user.
-            userRepository.deleteUserById(id);
+            // userRepository.deleteUserById(id, existUser.getUsername());
         };
     }
 
--- spring-aws-dynamodb/src/main/java/com/example/springawsdynamodb/UserRepository.java
+++ spring-aws-dynamodb/src/main/java/com/example/springawsdynamodb/UserRepository.java
@@ -1,12 +1,18 @@
 package com.example.springawsdynamodb;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBSaveExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Repository
+@Log4j2
 public class UserRepository {
 
     private final DynamoDBMapper dynamoDBMapper;
@@ -21,22 +27,55 @@ public class UserRepository {
     }
 
     public User findUserById(String id) {
-        return dynamoDBMapper.load(User.class, id);
+        Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
+        eav.put(":v1",new AttributeValue().withS(id));
+
+        DynamoDBQueryExpression<User> queryExpression = new DynamoDBQueryExpression<User>()
+                .withKeyConditionExpression("id = :v1")
+                .withExpressionAttributeValues(eav);
+
+        List<User> users = dynamoDBMapper.query(User.class,queryExpression);
+        return users.get(0);
+        // return dynamoDBMapper.load(User.class, id);
     }
 
-    public String deleteUserById(String id) {
-        User emp = dynamoDBMapper.load(User.class, id);
-        dynamoDBMapper.delete(emp);
-        return "Employee Deleted!";
+    void scanUserByBeginsWithUserName(String term){
+        HashMap<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
+        eav.put(":v1", new AttributeValue().withS(term));
+
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("begins_with(username,:v1)")
+                .withExpressionAttributeValues(eav);
+
+        List<User> users =  dynamoDBMapper.scan(User.class, scanExpression);
+       log.info(users.size());
     }
 
-    public String updateUserById(String id, User user) {
-        dynamoDBMapper.save(user,
-                new DynamoDBSaveExpression()
-                        .withExpectedEntry("id",
-                                new ExpectedAttributeValue(
-                                        new AttributeValue().withS(id)
-                                )));
+    void scanUserByAgeLessThan(int age){
+        HashMap<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
+        eav.put(":v1", new AttributeValue().withN(String.valueOf(age)));
+
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("age < :v1")
+                .withExpressionAttributeValues(eav);
+
+        List<User> users =  dynamoDBMapper.scan(User.class, scanExpression);
+        log.info(users.size());
+    }
+
+    public String deleteUserById(String id, String username) {
+        //User user = dynamoDBMapper.load(User.class, id);
+        User user = dynamoDBMapper.load(User.class, id, username);
+        dynamoDBMapper.delete(user);
+        return "User deleted!";
+    }
+
+    public String updateUser(String id, User user) {
+        var existUser = findUserById(id);
+        existUser.setAge(user.getAge());
+        existUser.setSurname(user.getSurname());
+        existUser.setAddress(user.getAddress());
+        dynamoDBMapper.save(existUser);
         return id;
     }
 }
